fddfd29a7e7752ba3e35ad596e799e368befbcfd
Malcolm <crummynz@gmail.com>
2015-07-31 14:24:17 +0200

    Added bar graph for last months worth of posts using D3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/// <reference path="typings/d3/d3.d.ts"/>
 /// <reference path="typings/angularjs/angular.d.ts"/>
 
 var chattyApp = angular.module('chattyApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
@@ -45,8 +46,101 @@ chattyApp.factory("Shacker", function($resource) {
   return $resource("http://do.malcolmcrum.com:4567/shacker/:name");
 });
 
-chattyApp.controller('homeController', function($scope) {
+chattyApp.factory("Post", function($resource) {
+  return $resource("http://do.malcolmcrum.com:4567/post/:id");
+})
+
+chattyApp.factory("Range", function($resource) {
+  return $resource("http://do.malcolmcrum.com:4567/from/:startYear/:startMonth/:startDay/to/:endYear/:endMonth/:endDay");
+})
+
+chattyApp.directive("rangeVisualization", function() {
+  var margin = { top: 40, right: 20, bottom: 60, left: 20};
   
+  return {
+    restrict: 'E',
+    scope: {
+      val: '='
+    },
+    link: function(scope, element, attrs) {
+      var vis = d3.select(element[0])
+        .append("svg")
+        .attr("id", "monthGraph")
+        .attr("height", "300px")
+        .attr("width", "100%");
+        
+      scope.$watch('val', function(newVal, oldVal) {
+        var width = document.getElementById("monthGraph").width.baseVal.value - margin.left - margin.right,
+            height = document.getElementById("monthGraph").height.baseVal.value - margin.top - margin.bottom;
+        
+        vis.selectAll('*').remove();
+        
+        if (!newVal) return;
+        
+        var formattedVal = [];
+        var i = 0;
+        for (var day in newVal.days) {
+          formattedVal.push({x: day, y: newVal.days[day], y0: 0});
+          i++;
+        }
+        
+        var days = formattedVal.length,
+            data = d3.layout.stack()([formattedVal]);
+            
+        var maxY = d3.max(formattedVal, function(d) { return d.y }),
+            x = function(d, i) { return margin.left + i * (width / days) },
+            barWidth = function(d) { return (width / days) - width/500 },
+            barHeight = function(d) { return d.y * height / maxY },
+            y = function(d) { return margin.top + height - barHeight(d) };
+          
+        var layers = vis.selectAll("g.layer")
+          .data(data)
+          .enter().append("g")
+            .style("fill", "blue")
+            .attr("class", "layer");
+            
+        var tooltip = d3.select("svg")
+          .append("text")
+          .text("tooltip")
+          .attr("class", "barlabel")
+          .style("visibility", "hidden")
+          .style("text-anchor", "middle")
+          .attr("y", height + margin.top + 20);
+        
+        var bars = layers.selectAll("g.bar")
+          .data(function(d) { return d; })
+          .enter().append("rect")
+            .attr("class", "bar")
+            .attr("width", barWidth)
+            .attr("x", x)
+            .attr("y", height)
+            .attr("height", 0)
+            .on("mouseover", function(d) {
+              d3.select(this).attr("fill", "green");
+              tooltip.style("visibility", "visible")
+                .attr("x", parseInt(d3.select(this).attr("x")) + parseInt(d3.select(this).attr("width")) /2)
+                .text(d.x);
+            })
+            .on("mouseout", function() {
+              d3.select(this).attr("fill", "");
+              tooltip.style("visibility", "hidden");
+            })
+          .transition()
+            .delay(function(d, i) { return i * 10; })
+            .attr("y", y)
+            .attr("height", barHeight);
+      })
+    }
+  }
+});
+
+chattyApp.controller('homeController', function($scope, Range) {
+  var today = new Date();
+  var lastMonth = new Date(today.valueOf() - 24*60*60*1000 * 30);
+  Range.get({startYear: lastMonth.getFullYear(), startMonth: lastMonth.getMonth()+1, startDay: lastMonth.getDate(),
+             endYear: today.getFullYear(), endMonth: today.getMonth()+1, endDay: today.getDate() }, function(data) {
+               $scope.monthPosts = data;
+             });
 });
 
 chattyApp.controller('dayController', function($scope, Day, Alerts, $routeParams) {
